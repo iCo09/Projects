d8a118f56f18f34ebaf2b5425327d7bb911f206e
Rahul Bisht <97499344+iCo09@users.noreply.github.com>
2024-10-14 13:12:04 +0530

    Create user.js

---
+++ Voting_App/models/user.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+//Define schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        require: true
+    },
+    email: {
+        type: String,
+    },
+    mobile: {
+        type: String
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    aadharCardNumber: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['voter', 'admin'],
+        default: 'voter'
+    },
+    isVoted: {
+        type: Boolean,
+        default: false
+    }
+});
+
+
+userSchema.pre('save', async function (next) {
+    const person = this;
+    //Hashed the password only if it has been modified (or is new)
+    if(!person.isModified('password')) return next();
+    try{
+        //hashed password generate
+        const salt = await bcrypt.genSalt(10);
+
+        //hash password
+        const hashed_password = await bcrypt.hash(person.password, salt);
+
+        person.password = hashed_password;
+
+        next();
+    }catch(err){
+        return next(err);
+    }
+})
+
+userSchema.methods.comparePassword = async function(candidatePassword){
+    try{
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    }catch(err){
+        throw err;
+    }
+}
+
+const User = mongoose.model('User',userSchema);
+module.exports = User;
