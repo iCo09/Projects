8d39958cea56887bf4975f46557aabaf8bc367d6
Rahul Bisht <97499344+iCo09@users.noreply.github.com>
2024-10-14 13:12:56 +0530

    Create userRoutes.js

---
+++ Voting_App/routes/userRoutes.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const{jwtAuthMiddleware, generateToken} = require('./../jwt');
+
+router.post('/signup', async (req,res) => {
+    try{
+        const data = req.body
+
+        const newUser = new User(data);
+
+        const response = await newUser.save();
+        console.log('data saved');
+
+
+        const payload = {
+            id: response.id,
+        }
+
+        console.log(JSON.stringify(payload));
+        const token = generateToken(payload);
+        console.log("Token is: ", token);
+
+        res.status(200).json({response: response, token: token});
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+        
+    }
+})
+
+//login route
+router.post('/login', async(req,res) => {
+    try{
+        const {aadharCardNumber,password} = req.body;
+        const user = await User.findOne({aadharCardNumber: aadharCardNumber});
+
+        if(!user || !(await user.comparePassword(password))){
+            return res.status(401).json({error: 'Invalid username or password'});
+        }
+
+        //generate tokens
+        const payload = {
+            id : user.id
+        }
+        const token = generateToken(payload);
+
+        res.json({token})
+    }catch(err){
+        console.error(err);
+        res.status(500).json({error : 'Internal Server Error'});
+    }
+})
+
+
+//profile route
+
+router.get('/profile', jwtAuthMiddleware,  async (req,res) => {
+    try{
+        const userData = req.user;
+        const userID = userData.id;
+        const user = await User.findById(userID);
+        res.status(200).json({user});
+    }catch(err){
+        console.error(err);
+        res.status(500).json({error : 'Internal Server Error'});
+    }
+});
+
+router.put('/profile/password', jwtAuthMiddleware, async (req,res)=>{
+    try{
+        const userId = req.user.id;
+        const {currentPassword, newPassword} = req.body
+
+        const user = await User.findById(userId);
+
+        if(!(await user.comparePassword(currentPassword))){
+            return res.status(401).json({error: 'Invalid username or password'});
+        }
+
+        user.password = newPassword;
+        await user.save();
+
+        console.log('Password Updated');
+        res.status(200).json({message: "Password Updated"});
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error:'Internal Server Error'});
+    }
+})
+
+module.exports = router;
